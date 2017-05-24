1d8f1036e8cf04c6a3e8fd270bfaa021951cf28d
MJ-sfo <michaeljlaird@yahoo.com>
2017-05-24 14:56:04 -0700

    login works-sees correct and incorrect login

--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,8 @@ Rails.application.routes.draw do
   get '/users/new', to: 'users#new', as: 'new_user'
   post '/users', to: 'users#create'
   get '/users/:id', to: 'users#show', as: 'user'
+  # now for login page
+  get '/login', to: 'sessions#new'
+  post '/sessions', to: 'sessions#create'
+  
 end
